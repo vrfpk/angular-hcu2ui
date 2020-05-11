cbf1669b1a53258416521d655b48332b8b726f62
vrfpk <m.wichulski@gmail.com>
2020-05-12 00:24:22 +0200

    chapter 2 display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,8 +10,13 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
